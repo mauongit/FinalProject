b5d10dea2499c4524d5326fafbe675c7e615f13a
mauongit <shukla.abi.69@gmail.com>
2015-11-02 04:39:35 -0800

    style changes

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -3,20 +3,21 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Autocomplete Application using Java and JQuery</title>
-<script src="/AddressValidationProj/scripts/jquery.js"></script>
-<script src="/AddressValidationProj/scripts/jquery-ui.js"></script>
-<script src="/AddressValidationProj/scripts/autocompleter.js"></script>
+<script src="/scripts/jquery.js"></script>
+<script src="/scripts/jquery-ui.js"></script>
+<script src="/scripts/autocompleter.js"></script>
 <link rel="stylesheet" 
-  href="/AddressValidationProj/css/jquery-ui.css">
+  href="/css/jquery-ui.css">
 <!-- User defied css -->
-<link rel="stylesheet" href="/AddressValidationProj/css/style.css">
+<link rel="stylesheet" href="/css/style.css">
 
 </head>
 <body>
-<img alt="verizon" style="height:32px;" src="/AddressValidationProj/images/vzw-logo-156-130-c.jpg">
+<img alt="verizon" style="height:32px;margin-top:200px" src="/images/vzw-logo-156-130-c.jpg">
 <div class="header">
         <h2>Please type below to search and validate the address</h2>
 </div>
+<span id="erroMessage" style="display:none;color:red">Please enter alphabet or number as the first character</span>
 <br />
 <br />
 <div class="search-container">
--- WebContent/scripts/autocompleter.js
+++ WebContent/scripts/autocompleter.js
@@ -2,67 +2,68 @@ var cache = {};
 
 
 $(document).ready(function() {
-        $(function() {
-                $("#search").autocomplete({     
-                source : function(request, response) {
-                	var term          = request.term.toLowerCase(),
-                    element       = this.element,
-                    cache         = this.element.data('autocompleteCache') || {},
-                    foundInCache  = false;
-                    //validation
-                    var validFormat = new RegExp('^[A-Za-z0-9]');
-                    //console.log(validFormat.test(term) + term);
-                    if(!validFormat.test(term)){
-                    	alert("Please enter alphabet or number as first character");
-                    	return;
-                    	}
+	$(function() {
+		$("#search").autocomplete({     
+			source : function(request, response) {
+				var term          = request.term.toLowerCase(),
+				element       = this.element,
+				cache         = this.element.data('autocompleteCache') || {},
+				foundInCache  = false;
+				//validation
+				var validFormat = new RegExp('^[A-Za-z0-9]');
+				//console.log(validFormat.test(term) + term);
+				if(!validFormat.test(term)){
+					$("#erroMessage").css("display","block");
+					return;
+				}
+				$("#erroMessage").css("display","none");
 
-                $.each(cache, function(key, data){
-                	
-                  if (term.indexOf(key) === 0 && data.length > 0) {
-                	  /*var start = new Date().getTime();*/
-                    var resultsArray =new Array();
-                    $.each(data, function( index, value ) {
-                   		if(value.toLowerCase().startsWith(term)){
-                   			resultsArray.push(value);
-                   		}
-                   	});
-                    response(resultsArray.slice(0,10));
-                    foundInCache = true;
-                    /*var end = new Date().getTime();
+				$.each(cache, function(key, data){
+
+					if (term.indexOf(key) === 0 && data.length > 0) {
+						/*var start = new Date().getTime();*/
+						var resultsArray =new Array();
+						$.each(data, function( index, value ) {
+							if(value.toLowerCase().startsWith(term)){
+								resultsArray.push(value);
+							}
+						});
+						response(resultsArray.slice(0,10));
+						foundInCache = true;
+						/*var end = new Date().getTime();
                     var time = end - start;
                     console.log('From Execution time: ' + time);*/
-                    return;
-                  }                  
-                });
-                  if (foundInCache) return;
-                $.ajax({
-                        url : "SearchController",
-                        type : "GET",
-                        data : {
-                                term : request.term.charAt(0)
-                        },
-                        dataType : "json",
-                        success : function(data) {
-                        	cache[term.charAt(0)] = data;
-                            element.data('autocompleteCache', cache);
-                           /* var start = new Date().getTime();*/
-                              var resultsArray =new Array();
-                              $.each(data, function( index, value ) {
-                             		if(value.toLowerCase().startsWith(term)){
-                             			resultsArray.push(value);
-                         			}
+						return;
+					}                  
+				});
+				if (foundInCache) return;
+				$.ajax({
+					url : "SearchController",
+					type : "GET",
+					data : {
+						term : request.term.charAt(0)
+					},
+					dataType : "json",
+					success : function(data) {
+						cache[term.charAt(0)] = data;
+						element.data('autocompleteCache', cache);
+						/* var start = new Date().getTime();*/
+						var resultsArray =new Array();
+						$.each(data, function( index, value ) {
+							if(value.toLowerCase().startsWith(term)){
+								resultsArray.push(value);
+							}
 
-                             	});
-                              response(resultsArray.slice(0,10));
-                              foundInCache = true;
-                              /*var end = new Date().getTime();
+						});
+						response(resultsArray.slice(0,10));
+						foundInCache = true;
+						/*var end = new Date().getTime();
                               var time = end - start;
                               console.log('Initial Cache Execution time: ' + time);*/
-                              return;     
-                        }
-                });
-            }
-        });
-    });
+						return;     
+					}
+				});
+			}
+		});
+	});
 });
