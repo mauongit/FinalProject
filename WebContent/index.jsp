c048927cfb2748453fc3b2a92375492b50ee9f9e
mauongit <shukla.abi.69@gmail.com>
2015-11-04 23:10:19 -0800

    commit

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -14,11 +14,12 @@
 </head>
 <body>
 <img style="height:165px;" src="/images/team-logo-156-130-c.jpg">
-<h1 style="position: absolute;top: 50px;left: 36%;font-weight: bold;font-family: sans-serif;">team</h1>
-<div class="header">
-        <h2>Please type below to search the address </h2>
+<h1 style="position: absolute;top: 50px;font-weight: bold;font-family: sans-serif;"> team</h1>
+<div class="header" style="margin-top:-34px">
+       <h2 style="font-family: sans-serif; text-align: center;">Please type below to search and validate the address</h2>
+        <h3 style="color:green;font-family: sans-serif; text-align: center;">(Auto populates the matching addresses from database and validates the same)</h3>
 </div>
-<span id="erroMessage" style="display:none;color:red">Please enter alphabet or number as the first character</span>
+<span id="erroMessage" style="display:none;color:red;font-family: sans-serif; text-align: center;"><h3>Please enter alphabet or number as the first character</h3></span>
 <br />
 <br />
 <div class="search-container">
@@ -27,5 +28,8 @@
         </div>
   		
 </div>
+<br />
+<span id="erroMessageNoAddress" style="display:none;color:red;font-family: sans-serif; text-align: center;"><h3>No Address found in our system, please search for a valid address.</h3></span>
+
 </body>
 </html>
--- WebContent/scripts/autocompleter.js
+++ WebContent/scripts/autocompleter.js
@@ -2,68 +2,90 @@ var cache = {};
 
 
 $(document).ready(function() {
-	$(function() {
-		$("#search").autocomplete({     
-			source : function(request, response) {
-				var term          = request.term.toLowerCase(),
-				element       = this.element,
-				cache         = this.element.data('autocompleteCache') || {},
-				foundInCache  = false;
-				//validation
-				var validFormat = new RegExp('^[A-Za-z0-9]');
-				//console.log(validFormat.test(term) + term);
-				if(!validFormat.test(term)){
-					$("#erroMessage").css("display","block");
-					return;
-				}
-				$("#erroMessage").css("display","none");
+        $(function() {
+                $("#search").autocomplete({     
+                source : function(request, response) {
+                	var term          = request.term.toLowerCase(),
+                    element       = this.element,
+                    cache         = this.element.data('autocompleteCache') || {},
+                    foundInCache  = false;
+                    //validation
+                    var validFormat = new RegExp('^[A-Za-z0-9]');
+                    //console.log("TErm::: " + term);
+                    if(!validFormat.test(term)){
+                    	$("#erroMessage").css("display","block");
+                    	 $("#erroMessageNoAddress").css("display","none");
+                    	return;
+                    	}
+                    $("#erroMessage").css("display","none");
 
-				$.each(cache, function(key, data){
-
-					if (term.indexOf(key) === 0 && data.length > 0) {
-						/*var start = new Date().getTime();*/
-						var resultsArray =new Array();
-						$.each(data, function( index, value ) {
-							if(value.toLowerCase().startsWith(term)){
-								resultsArray.push(value);
-							}
-						});
-						response(resultsArray.slice(0,10));
-						foundInCache = true;
-						/*var end = new Date().getTime();
+                $.each(cache, function(key, data){
+                	
+                  if (term.indexOf(key) === 0 && data.length > 0) {
+                	  /*var start = new Date().getTime();*/
+                    var resultsArray =new Array();
+                    $.each(data, function( index, value ) {
+                   		if(value.toLowerCase().startsWith(term)){
+                   			resultsArray.push(value);
+                   		}
+                   	});
+                    response(resultsArray.slice(0,10));
+                    foundInCache = true;
+                    //console.log("Array Length from Cache :"+resultsArray.length);
+                    if(resultsArray.length == 0){
+                    	//console.log("Array Length from Cache :"+resultsArray.length);
+                    	foundInCache = false;
+                    	return;
+                    }
+                    $("#erroMessageNoAddress").css("display","none");
+                    /*var end = new Date().getTime();
                     var time = end - start;
                     console.log('From Execution time: ' + time);*/
-						return;
-					}                  
-				});
-				if (foundInCache) return;
-				$.ajax({
-					url : "SearchController",
-					type : "GET",
-					data : {
-						term : request.term.charAt(0)
-					},
-					dataType : "json",
-					success : function(data) {
-						cache[term.charAt(0)] = data;
-						element.data('autocompleteCache', cache);
-						/* var start = new Date().getTime();*/
-						var resultsArray =new Array();
-						$.each(data, function( index, value ) {
-							if(value.toLowerCase().startsWith(term)){
-								resultsArray.push(value);
-							}
+                    return;
+                  }                  
+                });
+                //console.log("Value === "+foundInCache);
+                  if (foundInCache) return;
+                $.ajax({
+                        url : "SearchController",
+                        type : "GET",
+                        data : {
+                                term : request.term.charAt(0)
+                        },
+                        dataType : "json",
+                        success : function(data) {
+                        	cache[term.charAt(0)] = data;
+                            element.data('autocompleteCache', cache);
+                           /* var start = new Date().getTime();*/
+                              var resultsArray =new Array();
+                              $.each(data, function( index, value ) {
+                             		if(value.toLowerCase().startsWith(term)){
+                             			resultsArray.push(value);
+                         			}
 
-						});
-						response(resultsArray.slice(0,10));
-						foundInCache = true;
-						/*var end = new Date().getTime();
+                             	});
+                              response(resultsArray.slice(0,10));
+                             
+                              foundInCache = true;
+                              if(resultsArray.length<= 0){
+                            	  foundInCache = false;
+                            	  $("#erroMessageNoAddress").css("display","block");
+                              }else{
+                            	  $("#erroMessageNoAddress").css("display","none");
+                              }
+                              
+                              /*var end = new Date().getTime();
                               var time = end - start;
                               console.log('Initial Cache Execution time: ' + time);*/
-						return;     
-					}
-				});
-			}
-		});
-	});
+                              return;     
+                        }
+                });
+                if(!foundInCache){
+                	//console.log(3444);
+                	//$("#erroMessageNoAddress").css("display","block");
+                	return;
+                }
+            }
+        });
+    });
 });
